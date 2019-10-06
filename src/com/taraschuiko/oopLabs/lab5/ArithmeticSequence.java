a8ae3acc50ae40ae493801aa2e857b54ca5d7b0e
Taras Chuiko <taraschuiko@gmail.com>
2019-10-06 13:15:55 +0300

    made lab 5

---
+++ src/com/taraschuiko/oopLabs/lab5/ArithmeticSequence.java
@@ -0,0 +1,43 @@
+package com.taraschuiko.oopLabs.lab5;
+
+public class ArithmeticSequence extends Sequence {
+    private int start;
+    private int step;
+
+    public ArithmeticSequence(int start, int step, int size) {
+        super();
+        this.start = start;
+        this.step = step;
+        this.size = size;
+        this.values = new int[size];
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    @Override
+    public int getValue(int index) {
+        calculateSequence();
+        return values[index - 1];
+    }
+
+    public int calculateSum() {
+        calculateSequence();
+        int sum = 0;
+        for (int value: values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private void calculateSequence() {
+        for (int i = 0, n = 1; i < size; i++, n++) {
+            values[i] = start + (n - 1) * step;
+        }
+    }
+}
---
+++ src/com/taraschuiko/oopLabs/lab5/Sequence.java
@@ -0,0 +1,15 @@
+package com.taraschuiko.oopLabs.lab5;
+
+public class Sequence {
+    protected int size;
+    protected int[] values;
+
+    public void setSize(int size) {
+        this.size = size;
+        this.values = new int[size];
+    }
+
+    public int getValue(int index) {
+        return 0;
+    }
+}
